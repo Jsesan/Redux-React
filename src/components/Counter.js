386c5c33625e65f598849a14d271260bb2869c1f
Jsesan <jsesan00@gmail.com>
2021-08-30 15:26:55 -0300

    splitting index file

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch, connect } from "react-redux";
-import {counterActions} from "../store/index"
+import {counterActions} from "../store/counter"
 
 const Counter = () => {
   const dispatch = useDispatch();
