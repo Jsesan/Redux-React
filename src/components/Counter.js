b3d374f4972136a611ee3655152ce64972532a4d
Jsesan <jsesan00@gmail.com>
2021-08-30 14:07:19 -0300

    multiple states

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,10 @@ import { Component } from "react";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-  const toggleCounterHandler = () => {};
+  const showCounter = useSelector((state)=> state.showCounter)
+  const toggleCounterHandler = () => {
+    dispatch({type: "toggle"})
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -20,7 +23,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={decrementHandler}>Decrement</button>
         <button onClick={incrementHandler}>Increment</button>
